3eb1dc8052110de5d4d8fcbce1cca406fd738cd6
Huina1aosp <huinadiscordforhuina@gmail.com>
2024-10-22 17:12:54 +0900

    update

---
+++ evo.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/BoardConfigSoong.mk)
+$(call inherit-product, device/evolution/sepolicy/common/sepolicy.mk)
+-include vendor/evolution/build/core/config.mk
+
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#  ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_evo/tiramisu/ota.json
